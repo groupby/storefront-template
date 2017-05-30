175da756422544a3286ae3c79404f8171f51430a
Ben Teichman <ben.l.teichman@gmail.com>
2017-05-30 18:22:06 -0400

    transform products

--- src/products-zone/index.ts
+++ src/products-zone/index.ts
@@ -1,7 +1,25 @@
-import { view } from '@storefront/core';
+import { view, Component, ProductTransformer, Store, Structure } from '@storefront/core';
 import Zone from '../zone';
 
 @view('gb-products-zone', require('./index.html'))
-class ProductsZone extends Zone { }
+class ProductsZone extends Component {
+  structure: Structure = this.config.structure;
+  props: ProductsZone.Props;
+
+  onBeforeMount() {
+    const { products, ...zone } = this.props.zone;
+    this.state = {
+      ...zone,
+      products: products.map((product) => ProductTransformer.transform(product, this.structure))
+    };
+    this.expose('zone');
+  }
+}
+
+namespace ProductsZone {
+  export interface Props extends Zone.Props {
+    zone: Store.ProductsZone;
+  }
+}
 
 export default ProductsZone;
