58519db645359b2aa64d22207111d63c6f35d06b
Ben Teichman <ben.l.teichman@gmail.com>
2018-05-14 19:44:14 -0400

    improvements

--- test/unit/zone.ts
+++ test/unit/zone.ts
@@ -1,34 +1,21 @@
+import * as sinon from 'sinon';
 import Zone from '../../src/zone';
 import suite from './_suite';
 
-class MockZone extends Zone { }
+class MockZone extends Zone {}
 
 suite('Zone', ({ expect, spy }) => {
   let mockZone: MockZone;
 
-  beforeEach(() => mockZone = new MockZone());
+  beforeEach(() => (mockZone = new MockZone()));
 
   describe('init()', () => {
-    it('should expose props.zone', () => {
-      const zone: any = { a: 'b' };
-      const expose = mockZone.expose = spy();
-      mockZone.props = { zone };
+    it('should provide $zone alias', () => {
+      const provide = (mockZone.provide = spy());
 
       mockZone.init();
 
-      expect(expose).to.be.calledWith('zone', zone);
-    });
-  });
-
-  describe('onUpdate()', () => {
-    it('should call updateAlias()', () => {
-      const zone: any = { a: 'b' };
-      const updateAlias = mockZone.updateAlias = spy();
-      mockZone.props = { zone };
-
-      mockZone.onUpdate();
-
-      expect(updateAlias).to.be.calledWith('zone', zone);
+      expect(provide).to.be.calledWith('zone');
     });
   });
 });
